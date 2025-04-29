239dde3b3300e0890cd620d26bfaaed978e37b3e
sj <shijie.jh@gmail.com>
2025-04-30 01:21:56 +0800

    fix: trying to fix firebase deployment problem

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -5,15 +5,15 @@ import { getStorage } from 'firebase/storage';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAa2Y52RArOnL4DdmXjflTSIDanm2jSzn4",
-    authDomain: "chatroom-29fc3.firebaseapp.com",
-    databaseURL: "https://chatroom-29fc3-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "chatroom-29fc3",
-    storageBucket: "chatroom-29fc3.firebasestorage.app",
-    messagingSenderId: "789760414297",
-    appId: "1:789760414297:web:83b63d39c89bda3597d748",
-    measurementId: "G-FD7KCYNQ8R"
-  };
+  apiKey: "AIzaSyAa2Y52RArOnL4DdmXjflTSIDanm2jSzn4",
+  authDomain: "chatroom-29fc3.firebaseapp.com",
+  databaseURL: "https://chatroom-29fc3-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "chatroom-29fc3",
+  storageBucket: "chatroom-29fc3.firebasestorage.app",
+  messagingSenderId: "789760414297",
+  appId: "1:789760414297:web:83b63d39c89bda3597d748",
+  measurementId: "G-FD7KCYNQ8R"
+};
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
